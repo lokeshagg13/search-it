17c2fdaf6bdedc6abcb7c3520c0227ced5a2883b
= <=>
2023-07-01 22:17:47 +1000

    Minor updations in search result and comp

--- search-app/src/components/search/SearchComp.js
+++ search-app/src/components/search/SearchComp.js
@@ -1,23 +1,86 @@
 import { useState, useRef, useEffect } from "react";
 
+import axios from "../../api/axios";
+import Loader from "../ui/Loader";
+import Notification from "../ui/Notification";
+
 import classes from "./SearchComp.module.css";
 import SearchResult from "./SearchResult";
+import ImageOverlay from "./ImageOverlay";
+
+const SEARCH_IMAGE_URL = "/api/images/search";
+
+// Search image from backend server using a search expression provided by user
+async function searchImagesByTag(tag) {
+  let response;
+  try {
+    // Sending request to backend server for searching image
+    response = await axios.get(`${SEARCH_IMAGE_URL}/${tag}`);
+    console.log(response);
+    return { status: "SUCCESS", ...response.data };
+  } catch (error) {
+    if (response?.data) return { status: "FAILED", ...response.data };
+    else return { status: "FAILED", ...response };
+  }
+}
 
 // Search Component used on Search page with Search Input and Search Results
 function SearchComp() {
-  const [searchInput, setSearchInput] = useState("");
   const searchInputRef = useRef();
+  // For search input field
+  const [searchInput, setSearchInput] = useState("");
+  // For search tag value (value of search input field after clicking submit)
+  const [searchTag, setSearchTag] = useState(null);
+  // For checking if images are loaded or not
+  const [isLoading, setIsLoading] = useState(false);
+  // For storing image metadata
+  const [imageList, setImageList] = useState([]);
+  // In case of error
+  const [errorMsg, setErrorMsg] = useState(null);
+  // Selected Image
+  const [selectedImage, setSelectedImage] = useState(null);
 
   // Setting focus on search input when this component is loaded
   useEffect(() => {
     searchInputRef.current.focus();
   }, []);
 
-  // Handle submit of search, set searchInput state to the value of the search input field which will further fetch 
+  // Handle submit of search, set searchTag state to the value of the search input field which will further fetch
   // the search results from backend and update the SearchResult component
-  function submitHandler(event) {
+  async function submitHandler(event) {
     event.preventDefault();
-    setSearchInput(searchInputRef.current.value);
+    setIsLoading(true);
+    setSearchTag(searchInputRef.current.value);
+    let searchResult = await searchImagesByTag(searchInputRef.current.value);
+
+    // Checking and setting states based on api response
+    if (searchResult?.status === "SUCCESS") {
+      setErrorMsg(null);
+      setImageList(searchResult.photos.photo);
+    } else {
+      setImageList([]);
+      if (searchResult?.message) {
+        setErrorMsg(searchResult.message);
+      } else setErrorMsg("Error while searching for images. Please try again");
+    }
+    setIsLoading(false);
+  }
+
+  // Resetting the search
+  function resetHandler() {
+    setSearchInput("");
+    setSearchTag(null);
+    setImageList([]);
+  }
+
+  // Open Image Overlay
+  function openImageOverlay(image) {
+    setSelectedImage(image);
+  }
+
+  // Close Image Overlay
+  function closeImageOverlay() {
+    setSelectedImage(null);
   }
 
   // Component will contain search input, submit and reset button for submitting and resetting the search input and
@@ -31,18 +94,33 @@ function SearchComp() {
               type="text"
               id="search-input"
               autoComplete="off"
+              value={searchInput}
+              onChange={(event) => setSearchInput(event.target.value)}
               ref={searchInputRef}
             />
           </div>
           <div className={classes.actions}>
             <button type="submit">Search</button>
-            <button type="button" onClick={() => setSearchInput("")}>
+            <button type="button" onClick={resetHandler}>
               Reset
             </button>
           </div>
         </div>
       </form>
-      <SearchResult tag={searchInput} />
+      <>
+        {!errorMsg && !isLoading && (
+          <SearchResult
+            tag={searchTag}
+            images={imageList}
+            onImageSelect={openImageOverlay}
+          />
+        )}
+        {!errorMsg && isLoading && <Loader />}
+        {errorMsg && <Notification type="error" message={errorMsg} />}
+        {selectedImage && (
+          <ImageOverlay image={selectedImage} onClose={closeImageOverlay} />
+        )}
+      </>
     </>
   );
 }
